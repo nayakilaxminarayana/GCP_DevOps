e0197247435aa60df1b81c782d9333e903c61116
nayakilaxminarayana <47807616+nayakilaxminarayana@users.noreply.github.com>
2025-05-15 12:44:11 +0530

    Update main.py

--- gcp-cf-cloudbuild-main/main.py
+++ gcp-cf-cloudbuild-main/main.py
@@ -1,7 +1,19 @@
-import os
-from datetime import datetime
-import mysql.connector
+# import os
+# from datetime import datetime
+# import mysql.connector
 
-def return_basic(request):
-  now = datetime.now()
-  return '<h1>Welcome to Cloud Functions and Cloud Build demo</h1><br/>' + str(now)
+# def return_basic(request):
+#   now = datetime.now()
+#   return '<h1>Welcome to Cloud Functions and Cloud Build demo</h1><br/>' + str(now)
+
+
+def hello_world(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    return 'Hello, World!'
